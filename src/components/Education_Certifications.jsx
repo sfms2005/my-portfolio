abb16e2ee13294ab5e74186ad0bfd3ff2a7c6081
Sara Fawaz Alsubaie <sfms1426@gmail.com>
2025-09-30 18:11:20 +0300

    Update Education_Certifications.jsx

--- src/components/Education_Certifications.jsx
+++ src/components/Education_Certifications.jsx
@@ -11,15 +11,16 @@ function EducationCertifications() {
   };
 
   const certifications = [
-    {
-      title: 'Introduction to AI Engineering with Groq',
-      issuer: 'Tuwaiq',
-      date: 'Sep 2025',
-    },
+    
     {
       title: 'Microsoft Azure AI Fundamentals',
       issuer: 'Microsoft',
       date: 'Oct 2025',
+    },
+    {
+      title: 'Introduction to AI Engineering with Groq',
+      issuer: 'Tuwaiq',
+      date: 'Sep 2025',
     },
     {
       title: 'Basics of Building Websites from Scratch to Professionalism',
